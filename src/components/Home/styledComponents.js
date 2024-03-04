68ba6fbe6820117021946306bc4ae10df3a60671
vikaskumaryadav13 <vikasku664@gmail.com>
2024-03-04 06:01:21 +0000

    Travel Guide

---
+++ src/components/Home/styledComponents.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #eef4f7;
+  min-height: 100vh;
+`
+
+export const MainHeading = styled.h1`
+  font-family: 'Roboto';
+  font-weight: bold;
+  font-size: 31px;
+  border-bottom: 2.8px solid #52bbf0;
+  text-align: center;
+  color: #334155;
+  padding-bottom: 11px;
+`
+
+export const TravelGuideList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
---
+++ src/components/TravelPackageItem/styledComponents.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const TravelPackageListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  list-style-type: none;
+  width: 46%;
+  margin-right: 16px;
+  margin-bottom: 16px;
+`
+
+export const TravelPackageImage = styled.img``
+export const TravelInfoContainer = styled.div`
+  padding: 14px;
+`
+
+export const TravelPackageName = styled.h1`
+  font-family: 'Roboto';
+  font-weight: 550;
+  font-size: 21px;
+  color: #475569;
+`
+
+export const TravelPackageInfo = styled.p`
+  color: #64748b;
+  font-family: 'Roboto';
+  font-weight: 450;
+  font-size: 15px;
+`
